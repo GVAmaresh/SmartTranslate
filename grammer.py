5808afe7ec046b2711b399cde98667233e088fc2
GVAmaresh <eshama944@gmail.com>
2024-12-14 00:18:36 +0530

    dev: performace improved

--- grammer.py
+++ grammer.py
@@ -2,13 +2,13 @@
 from transformers import pipeline, T5ForConditionalGeneration, T5Tokenizer
 from happytransformer import HappyTextToText, TTSettings
 
-grammar_correction_model = pipeline(task="text2text-generation", model="hassaanik/grammar-correction-model")
+grammar_correction_model = pipeline(task="text2text-generation", model="hassaanik/grammar-correction-model", device=0)
 # happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
 args = TTSettings(num_beams=5, min_length=1)
 model_name = "samadpls/t5-base-grammar-checker"
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
-corrector = pipeline('text2text-generation', model="pszemraj/grammar-synthesis-small")
+corrector = pipeline('text2text-generation', model="pszemraj/grammar-synthesis-small", device=0)
 
 
 
